8686c996b8b9153c17b9eeb9b28bcf7c97fd407b
Erik Barron <todayisagrnday21@gmail.com>
2020-01-14 20:35:14 -0800

    add and remove cows via button

--- js/scripts.js
+++ js/scripts.js
@@ -2,4 +2,8 @@ $(document).ready(function() {
   $("button#addCow").click(function(){
     $("ul#cowPen").prepend("<img src='img/cow.png'>")
   })
+
+  $("button#minusCow").click(function() {
+    $("ul#cowPen").children("img").first().remove();
+  })
 })
